c5a99b5ac5ac050e420784f4436ca5e69a1a1535
spartan1cs <54705726+spartan1cs@users.noreply.github.com>
2025-05-15 11:40:38 +0530

    master 10 11

--- Abc.java
+++ Abc.java
@@ -22,8 +22,8 @@ public class Abc {
          System.out.print("chage by branch 1"); //master 4
 
 
-        System.out.print("chage by branch 1"); //commit 10
-        System.out.print("chage by branch 1"); //commit 11
+        System.out.print("chage by master 1"); //commit 10
+        System.out.print("chage by master 1"); //commit 11
 
 
     }
