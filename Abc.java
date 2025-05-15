e06fa0fb6480ebbc9a53a69fe85e9f8d9d15c31c
“Akash” <Akash@gmail.com>
2025-05-15 11:18:02 +0530

    commit 6

--- Abc.java
+++ Abc.java
@@ -3,6 +3,8 @@ public class Abc {
         System.out.print("chage by branch 1");//ok
         System.out.print("chage by branch 1");//commit 2
         System.out.print("chage by branch 1"); //commit 3
+        System.out.print("chage by branch 1"); //master1
+        System.out.print("chage by branch 1"); //master
 
 
     }
