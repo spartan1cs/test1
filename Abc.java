32d93a99015f7366e845a5ffe924155602b29012
spartan1cs <54705726+spartan1cs@users.noreply.github.com>
2025-06-25 17:59:38 +0530

    Update Abc.java

--- Abc.java
+++ Abc.java
@@ -1,35 +1,5 @@
 public class Abc {
     public static void main(String[] args) {
-        System.out.print("chage by branch 1");//ok
-        System.out.print("chage by branch 1");//commit 2
-        System.out.print("chage by branch 1"); //commit 3
-        System.out.print("chage by branch 1"); //master1
-        System.out.print("chage by branch 1"); //master
-        System.out.print("chage by branch 1"); //commit 4
-        System.out.print("chage by branch 1"); //commit 5
-        System.out.print("chage by branch 1"); //commit 6
-        System.out.print("chage by branch 1"); //commit 7
-        System.out.print("chage by branch 1"); //commit 8
-        System.out.print("chage by branch 1"); //commit 9
-
-
-         System.out.print("chage by branch 1"); //master 2
-                System.out.print("chage by branch 1"); //master 2
-         System.out.print("chage by branch 1"); //master 3
-         System.out.print("chage by branch 1"); //master 3
-
-         System.out.print("chage by branch 1"); //master 4
-         System.out.print("chage by branch 1"); //master 4
-
-
-        System.out.print("chage by master 1"); //commit 10
-        System.out.print("chage by master 1"); //commit 11
-        System.out.print("chage by branch 1"); //commit 12
-        System.out.print("chage by branch 1"); //commit 13
-        System.out.print("chage by branch 1"); //commit 13
-
-        akash changes
-
-        spartan commit
+     
     }
 }
